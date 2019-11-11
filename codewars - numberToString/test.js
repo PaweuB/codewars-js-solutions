c3a36cc22f89e6b18e9c12a928e7633904ff7cf8
PaweuB <spinacz@gmail.com>
2019-11-11 19:34:20 +0100

    numToString codewars kata testcases update

--- codewars - numberToString/test.js
+++ codewars - numberToString/test.js
@@ -1,1 +1,5 @@
 Test.assertEquals(numberToString(67), '67');
+Test.assertEquals(numberToString(79585), '79585');
+Test.assertNotEquals(numberToString(79585), 79585);
+Test.assertEquals(numberToString(1+2), '3');
+Test.assertEquals(numberToString(1-2), '-1');
